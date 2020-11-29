78e565a55a4c25b5af373a7d266839ff6e86ec88
Tarun Gorai <goraitarunkumar@gmail.com>
2020-11-29 09:46:58 +0530

    implemented two way binding

--- src/App.js
+++ src/App.js
@@ -4,21 +4,42 @@ import './App.css';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
 
-class App extends React.Component{
-  render() {
-    const user = {
-      name: "Anna",
-      hobbies: ["Sports"],
-    };
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      homeLink: "Home",
+    };
+  }
+
+  onGreet() {
+    alert("Hello!");
+  }
+
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName,
+    });
+  }
+
+  render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Header />
+            <Header homeLink={this.state.homeLink} />
           </div>
+        </div>
+        <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home name={"Tarun"} initialAge={26} user={ user }/>
+            <Home
+              name={"Max"}
+              initialAge={27}
+              greet={this.onGreet}
+              changeLink={this.onChangeLinkName.bind(this)}
+              initialLinkName={this.state.homeLink}
+            />
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,66 @@
 import React from "react";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            status: 0,
-        }
-    }
-    onMakeOlder() {
-        this.setState({ age: this.state.age + 3 });
-        console.log(this.age)
-    }
-    render() {
-      return (
-          <div>
-              <p> In a new content!</p>
-              <p>Your name is {this.props.name}</p>
-              <p>Your age is {this.state.age}</p>
-              <p>status:{this.state.status}</p>
-              <p>User obj {this.props.user.name}</p>
-              <button className="btn btn-primary" onClick={ this.onMakeOlder.bind(this) }>Make me older</button>
-        </div>
+  constructor(props) {
+    super();
+    this.state = {
+      age: props.initialAge,
+      status: 0,
+      homeLink: props.initialLinkName,
+    };
+    setTimeout(() => {
+      this.setState({
+        status: 1,
+      });
+    }, 3000);
+  }
+
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 3,
+    });
+  }
+
+  onChangeLink() {
+    this.props.changeLink(this.state.homeLink);
+  }
+
+  onHandleChange(event) {
+    this.setState({
+      homeLink: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>In a new Component!</p>
+        <p>
+          Your name is {this.props.name}, your age is {this.state.age}
+        </p>
+        <p>Status: {this.state.status}</p>
+        <hr />
+        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">
+          Make me older!
+        </button>
+        <hr />
+        <button onClick={this.props.greet} className="btn btn-primary">
+          Greet
+        </button>
+        <hr />
+        <input
+          type="text"
+          value={this.state.homeLink}
+          onChange={(event) => this.onHandleChange(event)}
+        />
+        <button
+          onClick={this.onChangeLink.bind(this)}
+          className="btn btn-primary"
+        >
+          Change Header Link
+        </button>
+      </div>
     );
   }
 }
+

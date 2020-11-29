00f2f74ce5481f596419224c1c9760bd9c2872c9
Tarun Gorai <goraitarunkumar@gmail.com>
2020-11-29 09:15:24 +0530

    handling event

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+    onMakeOlder() {
+        this.age += 3
+    }
     render() {
-      console.log(this.props)
       return (
           <div>
               <p> In a new content!</p>
               <p>Your name is {this.props.name}</p>
+              <p>Your age is {this.age}</p>
               <p>User obj {this.props.user.name}</p>
               <div>
                   <h4>Hobbies</h4>
@@ -16,6 +23,7 @@ export class Home extends React.Component {
                       })}
                   </ul>
               </div>
+              <button className="btn btn-primary" onClick={ this.onMakeOlder.bind(this) }>Make me order</button>
         </div>
     );
   }
